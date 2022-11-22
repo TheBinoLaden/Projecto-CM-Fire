3bd00279eeb7125b181d0b3a3aa7efdc14c9e41b
DianaMSJesus <83040508+DianaMSJesus@users.noreply.github.com>
2022-11-22 22:51:54 +0000

    Botão back
    já é possivel voltar das moradas para o mapa

--- FinalProject/app/src/main/java/com/example/finalproject/MineAdresses.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/MineAdresses.kt
@@ -1,18 +1,26 @@
 package com.example.finalproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 class MineAdresses : AppCompatActivity() {
 
     lateinit var adressFragment: AdressFragment
+    lateinit var toolbarInfo : Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mine_adresses)
 
         adressFragment = AdressFragment()
+
+        toolbarInfo = findViewById(R.id.myToolBar)
+        toolbarInfo.setNavigationOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
 
         val fragment : Fragment? = supportFragmentManager.findFragmentByTag(AdressFragment::class.java.simpleName)
         if(fragment !is AdressFragment){
