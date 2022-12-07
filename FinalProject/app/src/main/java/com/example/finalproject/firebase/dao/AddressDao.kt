00cd8092d605a69d32203c611958501757e2d0a9
rodrigo.albino <rodrigoalbino96@live.com.pt>
2022-12-07 18:13:10 +0000

    Added connection between addresses and other components.

--- FinalProject/app/src/main/java/com/example/finalproject/firebase/dao/AddressDao.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/firebase/dao/AddressDao.kt
@@ -1,12 +1,23 @@
 package com.example.finalproject.firebase.dao
 
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 class AddressDao {
 
 
-    companion object{
+    companion object {
 
-       fun addNewAddress(){
+        fun addNewAddress(username: String, newList: String) {
 
+            val dbConnection = Firebase.firestore
+            val dataToUpdate = hashMapOf(
+                "favPlaces" to newList
+
+            )
+            dbConnection.collection("users").document(username)
+                .set(dataToUpdate, SetOptions.merge())
         }
     }
 }
--- FinalProject/app/src/main/java/com/example/finalproject/firebase/utils/AddressUtils.kt
+++ FinalProject/app/src/main/java/com/example/finalproject/firebase/utils/AddressUtils.kt
@@ -1,11 +1,44 @@
 package com.example.finalproject.firebase.utils
 
+import android.view.LayoutInflater
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.view.children
+import com.example.finalproject.AddressActivity
+import com.example.finalproject.R
+import com.example.finalproject.firebase.dao.AddressDao
+
+
 class AddressUtils {
 
 
-    companion object{
+    companion object {
 
-        fun addNewAddress(){
+        fun addNewAddress(
+            username: String,
+            favPlaces: String,
+            address: String,
+            description: String
+        ) {
+
+            val fullAddress = "$address:$description"
+            val newList = favPlaces + fullAddress
+
+            AddressDao.addNewAddress(username, newList)
+        }
+
+        fun handleAddressList(activity: AddressActivity, table: TableLayout) {
+
+            for (b in table.children) {
+                // Inflate your row "template" and fill out the fields.
+                val row: TableRow = LayoutInflater.from(activity)
+                    .inflate(R.layout.activity_adresses, null) as TableRow
+                (row.findViewById(R.id.casa) as TextView).text = "test"
+                (row.findViewById(R.id.morada) as TextView).text = "Testlol"
+                table.addView(row)
+            }
+            table.requestLayout()
 
         }
     }
